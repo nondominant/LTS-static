594a87b5e171758e51616c1a6a3feca20399cc57
nondominant <peter_hooper@hotmail.co.nz>
2021-11-29 16:19:06 +1300

    added utils.css

--- pages/components/index.js
+++ pages/components/index.js
@@ -3,7 +3,6 @@ export { default as Brand } from './brand/Brand';
 export { default as ContactBanner } from './contactBanner/ContactBanner';
 export { default as EmailLinkBar } from './emailLinkBar/EmailLinkBar';
 export { default as Gallery } from './gallery/Gallery';
-export { default as Navbar } from './navbar/Navbar';
 export { default as Logo } from './logo/Logo';
 export { default as QuotePanel } from './quotePanel/QuotePanel';
 export { default as TextFeature } from './textFeature/TextFeature';
