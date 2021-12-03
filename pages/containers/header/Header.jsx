6e4dbc487f55d7a2cbaa6e74707f4ca6410e0ebd
nondominant <peter_hooper@hotmail.co.nz>
2021-12-03 14:44:19 +1300

    swap

--- pages/containers/header/Header.jsx
+++ pages/containers/header/Header.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { Brand, ContactBanner, Logo, ImagePanel } from '../../components';
 import style from './header.module.css';
 
 
-const Header = () => (
+const Header = () => {
+
+  const [scroll, setScroll] = useState(false);
+   useEffect(() => {
+     window.addEventListener("scroll", () => {
+       setScroll(window.scrollY > 100);
+       console.log("window.scrollY", window.scrollY);
+     });
+   }, []);
+
+  //className={scroll ? "bg-black" : "bg-white"}
+  return (
   <>
-    <div className={style.header_component}>
+    <div className={!scroll 
+      ? [style.header_absolute, style.opaque].join(" ")
+      : [style.header_component, style.opaque].join(" ")
+    }>
           <div className={style.header_item}><Link href="/"><a>Home</a></Link></div>
           <div className={style.header_item}><Link href="/about"><a>About</a></Link></div>
           <div className={style.header_item}><Link href="/contact"><a>Contact</a></Link></div>
     </div>
   </>
-);
+  );
+}
 
 export default Header;
