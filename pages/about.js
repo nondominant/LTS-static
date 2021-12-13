618c5afde41ce9ea8d53ebe7580474170b9a38fe
nondominant <peter_hooper@hotmail.co.nz>
2021-12-14 08:26:47 +1300

    tuesday 14th start of day commit before refactoring imagepanel to accept props

--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,51 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Header, Footer, ContentPanel } from './containers';
 import { RelativeLinkMenu } from './components';
+import style from './index.module.css';
 
 export default function About() {
+  const links = [
+    "#company",
+    "#team"
+  ]
+  const names = [
+    "Company",
+    "Team"
+  ]
+
   return (
     <>
+    <div className={style.main}>
     <Header />
+    <div className={style.body}>
+    {/* <PageTitle id="company"/> */}
+    {/* <CardPanel /> */}
+    {/* <ContentPanel /> */}
+    {/* <FlexGallery /> */}
+    
+
+    {/* <PageTitle id="team"/> */}
+    {/* <CardPanel /> */}
+    {/* <ContentPanel /> */}
+    {/* <FlexGallery /> */}
+
+    <div>
+    <RelativeLinkMenu className={style.a} names={names} links={links} id="#company"/>
     <ContentPanel />
     <ContentPanel />
-    <RelativeLinkMenu />
+    <ContentPanel />
+    <ContentPanel />
+    <ContentPanel id="team"/>
+    <ContentPanel />
+    <ContentPanel />
+    <ContentPanel />
     <ContentPanel />
     <ContentPanel id="company"/>
+    </div>
+    </div>
+
     <Footer />
+    </div>
     </>
   );
 }
--- pages/components/relativeLinkMenu/RelativeLinkMenu.jsx
+++ pages/components/relativeLinkMenu/RelativeLinkMenu.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import style from './relativeLinkMenu.module.css';
-import { HashLink } from 'react-router-hash-link';
+import { Component } from 'react';
+import { ContentPanel } from '../../containers';
+import Link from 'next/link';
 
-const RelativeLinkMenu = () => {
-  return (
-    <div className={style.container}>
-    <div><HashLink to="/about#company">Company</HashLink></div>
-    <div></div>
-    </div>
-  );
-}
+const RelativeLinkMenu = (props) => {
+    console.log(`${props.links}`)
+    console.log(`${props.names}`)
+    return (
+      <>
+      <div className={style.container}>
+      <div className={style.background}>
+      {
+        props.links.map((link, i) => 
+        <div className={style.link}>
+          <Link href={link}>
+          <a>{`${props.names[i]}`}</a>
+          </Link>
+          </div>)
+      } 
+      </div>
+      </div>
+      </>
+    );
+};
 
 export default RelativeLinkMenu;
+      //<div><Link href={`${props.id}`}><a>Company</a></Link></div>
