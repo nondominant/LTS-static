9c2220eb26dfd7dee7e165f46e9d519a97387614
nondominant <peter_hooper@hotmail.co.nz>
2022-02-18 13:08:19 +1300

    implemented radio button menu to replace header

--- pages/containers/header/Header.jsx
+++ pages/containers/header/Header.jsx
@@ -46,22 +46,108 @@ const Header = () => {
   return (
 <>
 <div className={!scroll 
-  ? [style.header_fat, style.header_fixed, style.grid_layout, style.opaque, (scrollStatus.scrollDirection === "up" 
+  ? [style.header_fat, style.header_fixed, style.opaque, (scrollStatus.scrollDirection === "up" 
     ? style.red
     : style.blue)].join(" ")
-  : [style.header_thin, style.header_fixed, style.grid_layout_thin, style.opaque, (scrollStatus.scrollDirection === "up" 
+  : [style.header_thin, style.header_fixed, style.opaque, (scrollStatus.scrollDirection === "up" 
     ? style.red 
     : style.blue)].join(" ")
 }>
 
 
   <div className={style.naviGAYtion}>
-  <input className={style.inputA} id="check01" type="checkbox" name="menu" />
-  <label for="check01">Menu</label>
-  <ul className={style.submenu}>
-    <li><a href="#">Item 1</a></li>
-    <li><a href="#">Item 2</a></li>
-  </ul>
+    <div>
+      <input type="radio" name="menu" id="check01" />
+      <label for="check01">Menu</label>
+      <ul className={style.submenu}>
+        <li><a href="#">Item 1</a></li>
+        <li><a href="#">Item 2</a></li>
+        <input type="radio" name="menu" id="close" />
+        <label className={style.close} for="close">close</label>
+      </ul>
+    </div>
+    <div>
+      <input type="radio" name="menu" id="check011" />
+      <label for="check011">Menu</label>
+      <ul className={style.submenu}>
+        <li><a href="#">Item 1</a></li>
+        <li><a href="#">Item 2</a></li>
+        <input type="radio" name="menu" id="close" />
+        <label className={style.close} for="close">close</label>
+      </ul>
+    </div>
+    <div>
+      <input type="radio" name="menu" id="check012" />
+      <label for="check012">Menu</label>
+      <ul className={style.submenu}>
+        <li><a href="#">Item 1</a></li>
+        <li><a href="#">Item 2</a></li>
+        <input type="radio" name="menu" id="close" />
+        <label className={style.close} for="close">close</label>
+      </ul>
+    </div>
+    <div>
+      <input type="radio" name="menu" id="check013" />
+      <label for="check013">Menu</label>
+      <ul className={style.submenu}>
+        <li><a href="#">Item 1</a></li>
+        <li><a href="#">Item 2</a></li>
+        <input type="radio" name="menu" id="close" />
+        <label className={style.close} for="close">close</label>
+      </ul>
+    </div>
+
+    <div>
+      <input type="radio" name="menu" id="check02" />
+      <label for="check02">Menu</label>
+      <ul className={style.submenu}>
+        <li>
+
+          <div className={[style.naviGAYtion, style.column].join(" ")}>
+            <div>
+              <input type="checkbox" name="submenu" id="check04" />
+              <label for="check04">Menu</label>
+              <ul className={style.subsubmenu}>
+                <li><a href="#">Item 1</a></li>
+                <li><a href="#">Item 2</a></li>
+              </ul>
+            </div>
+            <div>
+              <input type="checkbox" name="submenu" id="check05" />
+              <label for="check05">Menu</label>
+              <ul className={style.subsubmenu}>
+                <li><a href="#">Item 1</a></li>
+                <li><a href="#">Item 2</a></li>
+              </ul>
+            </div>
+            <div>
+              <input type="checkbox" name="submenu" id="check06" />
+              <label for="check06">Menu</label>
+              <ul className={style.subsubmenu}>
+                <li><a href="#">Item 1</a></li>
+                <li><a href="#">Item 2</a></li>
+              </ul>
+            </div>
+          </div>
+
+
+          </li>
+
+        <li><a href="#">Item 2</a></li>
+        <input type="radio" name="menu" id="close" />
+        <label className={style.close} for="close">close</label>
+      </ul>
+    </div>
+    <div>
+      <input type="radio" name="menu" id="check03" />
+      <label for="check03">Menu</label>
+      <ul className={style.submenu}>
+        <li><a href="#">Item 1</a></li>
+        <li><a href="#">Item 2</a></li>
+        <input type="radio" name="menu" id="close" />
+        <label className={style.close} for="close">close</label>
+      </ul>
+    </div>
   </div>
 
 
