b039310bf9e08c590aff479ea8d7f6e815074f6d
nondominant <peter_hooper@hotmail.co.nz>
2022-02-23 10:27:40 +1300

    modified header

--- js/containers/header/Header.jsx
+++ js/containers/header/Header.jsx
@@ -59,99 +59,45 @@ const Header = () => {
   <div className={[style.header_image, style.logo_pos].join(" ")}>
     <img className={style.image} src={transparent_logo}/></div>
     <div>
-      <input type="radio" name="menu" id="check01" />
-      <label for="check01">Menu</label>
-      <ul className={style.submenu}>
-        <li><a href="#">Item 1</a></li>
-        <li><a href="#">Item 2</a></li>
-        <input type="radio" name="menu" id="close" />
-        <label className={style.close} for="close">close</label>
-      </ul>
+        <Link href="/"><a>Home</a></Link>
     </div>
     <div>
-      <input type="radio" name="menu" id="check011" />
-      <label for="check011">Menu</label>
-      <ul className={style.submenu}>
-        <li><a href="#">Item 1</a></li>
-        <li><a href="#">Item 2</a></li>
-        <input type="radio" name="menu" id="close" />
-        <label className={style.close} for="close">close</label>
-      </ul>
+        <Link href="/about"><a >About Us</a></Link>
     </div>
     <div>
-      <input type="radio" name="menu" id="check012" />
-      <label for="check012">Menu</label>
-      <ul className={style.submenu}>
-        <li><a href="#">Item 1</a></li>
-        <li><a href="#">Item 2</a></li>
-        <input type="radio" name="menu" id="close" />
-        <label className={style.close} for="close">close</label>
-      </ul>
+        <Link href="/contact"><a >Contact</a></Link>
     </div>
     <div>
       <input type="radio" name="menu" id="check013" />
-      <label for="check013">Menu</label>
+      <label htmlFor="check013">Menu</label>
       <ul className={style.submenu}>
-        <li><a href="#">Item 1</a></li>
-        <li><a href="#">Item 2</a></li>
+        <li><Link href="/spareparts"><a >Spare Parts</a></Link></li>
+        <li><Link href="/machinerysales"><a >Machinery Sales</a></Link></li>
+        <li><Link href="/maintenance"><a >Maintenance</a></Link></li>
+        <li><Link href="/projectmanagement"><a >Project Management</a></Link></li>
+        <li><Link href="/controlsystems"><a >Control Systems</a></Link></li>
+        <li><Link href="/installations"><a >Installation</a></Link></li>
+        <li><Link href="/plantrelocation"><a >Plant Relocation</a></Link></li>
+        <li><Link href="/monorail"><a >Monorail</a></Link></li>
+        <li><Link href="/maritime"><a >Maritime</a></Link></li>
         <input type="radio" name="menu" id="close" />
-        <label className={style.close} for="close">close</label>
-      </ul>
-    </div>
-
-    <div>
-      <input type="radio" name="menu" id="check02" />
-      <label for="check02">Menu</label>
-      <ul className={style.submenu}>
-        <li>
-
-          <div className={[style.naviGAYtion, style.column].join(" ")}>
-            <div>
-              <input type="checkbox" name="submenu" id="check04" />
-              <label for="check04">Menu</label>
-              <ul className={style.subsubmenu}>
-                <li><a href="#">Item 1</a></li>
-                <li><a href="#">Item 2</a></li>
-              </ul>
-            </div>
-            <div>
-              <input type="checkbox" name="submenu" id="check05" />
-              <label for="check05">Menu</label>
-              <ul className={style.subsubmenu}>
-                <li><a href="#">Item 1</a></li>
-                <li><a href="#">Item 2</a></li>
-              </ul>
-            </div>
-            <div>
-              <input type="checkbox" name="submenu" id="check06" />
-              <label for="check06">Menu</label>
-              <ul className={style.subsubmenu}>
-                <li><a href="#">Item 1</a></li>
-                <li><a href="#">Item 2</a></li>
-              </ul>
-            </div>
-          </div>
-
-
-          </li>
-
-        <li><a href="#">Item 2</a></li>
-        <input type="radio" name="menu" id="close" />
-        <label className={style.close} for="close">close</label>
+        <label className={style.close} htmlFor="close">close</label>
       </ul>
     </div>
     <div>
+        <Link href="/asia"><a >Asia</a></Link>
     </div>
+
     <div>
       <input type="radio" name="menu" id="check03" />
-      <label for="check03">Menu</label>
+      <label htmlFor="check03">Login</label>
       <ul className={style.submenu}>
-        <li><a href="#">Item 1</a></li>
-        <li><a href="#">Item 2</a></li>
+        <li><Link href="/"><a >Customer Login</a></Link></li>
+        <li><Link href="/"><a >Web Shop</a></Link></li>
 
         <li>
         <input type="radio" name="menu" id="close" />
-        <label className={style.close} for="close">close</label>
+        <label className={style.close} htmlFor="close">close</label>
         </li>
 
       </ul>
