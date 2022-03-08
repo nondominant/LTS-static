e7e497ab474d41363ccd039074344e7f08a98e33
nondominant <peter_hooper@hotmail.co.nz>
2022-03-08 14:41:34 +1300

    modified main page to default layout

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import style from './index.module.css';
 
+import img1 from '../public/static/LTSimages/MT200.jpg';
+import img2 from '../public/static/LTSimages/AT230.jpg';
+import img3 from '../public/static/LTSimages/BT300.jpg';
+import img4 from '../public/static/LTSimages/dryer.jpg';
 import { Header, Footer, ContentPanel, GalleryPanel } from '../js/containers';
 import { 
   Brand, 
@@ -14,7 +18,12 @@ import {
   FeaturePanel_locations, 
   Text_Overlay,
   RelativeLinkOverlay,
-  FeaturePanel_contact
+  FeaturePanel_contact,
+  Spacer,
+  Rowbox,
+  StaticImage,
+  BlockSpacer,
+  Columnbox
 } from '../js/components';
 
 export default function Home() {
@@ -39,8 +48,46 @@ export default function Home() {
     <div className={style.main}>
     <Header />
     <TitleImagePanel objectProps={{textLarge, textSmall}}/>
+
     <div className={style.body}>
-    <GalleryPanel />
+      <BlockSpacer height="20vh" color="white"/>
+        <div className={[style.slider__parent].join(" ")}>
+          <div className={style.slider__child__30}>
+
+
+          </div>
+          <div className={style.slider__relative}>
+
+          <BlockSpacer color="linear-gradient(180deg, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 100%);" height="300px"/>
+          <div className={style.opaque__white}>
+          <ImagePanel image={img4} justify="center" contentWidth="90">
+
+
+          </ImagePanel>
+
+
+          <BlockSpacer color="transparent;" height="500px"/>
+          <Rowbox objectProps={{justify: "center"}}>
+          <Columnbox>
+
+            <StaticImage src={img1} />
+
+          </Columnbox>
+          <Columnbox>
+
+            <StaticImage src={img2} />
+
+          </Columnbox>
+          <Columnbox>
+
+            <StaticImage src={img3} />
+
+          </Columnbox>
+          </Rowbox>
+          <Spacer />
+          </div>
+        </div>
+      </div>
     </div>
     <Linebreak />
     <Footer />
